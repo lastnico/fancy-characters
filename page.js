5eb36c69885b328de7f7632e4ec6472673675125
Nicolas Ternisien <nicolas.ternisien@gmail.com>
2011-09-20 20:07:36 +0200

    Merge with online changes

--- page.js
+++ page.js
@@ -2,7 +2,36 @@ var jaiqueray = jQuery.noConflict();
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	if (request.action == "insert-character") {
-		document.activeElement.value = document.activeElement.value + request.content;
+		var activeElement = document.activeElement;
+		if (! activeElement) {
+			return;
+		}
+		
+		var editable;
+		if (activeElement.nodeName == "IFRAME") {
+			// TODO Check if name exists, if not, maybe try with id
+			// TODO Make it work for iframe inside iframe inside...
+			editable = window.frames[activeElement.name].document.activeElement;
+		}
+		else {
+			// TODO Check this is an input or a textarea
+			editable = activeElement;
+		}
+		
+		
+		if (editable && editable.value == "") {
+			editable.value = request.content;
+		}
+		else if (editable) {
+			var oldtext = editable.value;
+			var curpos = editable.selectionStart;
+			pretext = oldtext.substring(0, curpos);
+			posttest = oldtext.substring(curpos,oldtext.length);
+			editable.value = pretext + request.content + posttest;
+		}
+		
+		// TODO Maybe add a highlight effect?
+		
 	}
 	else if (request.action == "get-font") {
 		sendResponse( { 
